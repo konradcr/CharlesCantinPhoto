8e61194be132751d3b474b5825e66ee371126a40
konradcr <63312007+konradcr@users.noreply.github.com>
2022-02-25 13:35:01 +0100

    Eleventy initialisation

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+/* Config file */
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/img");
+    eleventyConfig.addPassthroughCopy("./src/js");
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+};
